96581309daa680c503cd67ed39c581984bd03f53
PettoDavida <maximilian.widman@gmail.com>
2023-03-08 13:44:22 +0100

    landing page query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,76 @@
 import * as React from "react";
 import {graphql, HeadFC, PageProps, useStaticQuery} from "gatsby";
-import Button from "../library/Button";
-import {Switch, useColorMode} from "theme-ui";
-
-interface PageData {
-  allStrapiContent: {
-    nodes: [
-      {
-        Heading: string;
-      }
-    ];
-  };
-}
+import {Box} from "theme-ui";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import "../css/global.css";
+import PageSection from "../components/pagesection";
+import {landingPageData} from "../interfaces/types";
 
 const IndexPage: React.FC<PageProps> = () => {
-  const [mode, setMode] = useColorMode();
-
-  const data: PageData = useStaticQuery(
+  const indexQuery: landingPageData = useStaticQuery(
     graphql`
       query {
-        allStrapiContent {
-          nodes {
-            Heading
+        strapiPage(pagename: {eq: "landing"}) {
+          contents {
+            heading {
+              title
+              position
+              variant
+            }
+            subheading {
+              title
+              position
+              variant
+            }
+            description {
+              title
+              position
+              variant
+            }
+            position
+            section
+            compType
+            services {
+              servicename
+              img {
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(width: 387, formats: [AUTO])
+                  }
+                }
+              }
+              title {
+                title
+                position
+                variant
+              }
+              description {
+                title
+                position
+                variant
+              }
+            }
+            img {
+              localFile {
+                url
+              }
+            }
           }
         }
       }
     `
   );
+  console.log(indexQuery);
 
   return (
-    <div>
-      <Switch
-        onClick={() => {
-          setMode(mode === "dark" ? "default" : "dark");
-        }}
-      ></Switch>
-      <h1>Strapi blog</h1>
-      {data.allStrapiContent.nodes.map((item, i) => {
-        return <h2 key={i}>{item.Heading}</h2>;
+    <Box>
+      <Header />
+      {indexQuery.strapiPage.contents.map((item, i) => {
+        return <PageSection data={item} key={i} />;
       })}
-      <Button>testing</Button>
-    </div>
+      <Footer />
+    </Box>
   );
 };
 
